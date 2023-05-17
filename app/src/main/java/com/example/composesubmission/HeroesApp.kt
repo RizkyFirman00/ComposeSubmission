5e1f66818b5fefc9d8c4a8fdb8b17bb4a9942640
Damantine <firmansyah.mrf@gmail.com>
2023-05-17 09:31:31 +0700

    Fixing ScrollAppBar Commit

--- app/src/main/java/com/example/composesubmission/HeroesApp.kt
+++ app/src/main/java/com/example/composesubmission/HeroesApp.kt
@@ -1,5 +1,6 @@
 package com.example.composesubmission
 
+import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,16 +25,21 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -46,6 +53,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -68,6 +76,8 @@ fun JetHeroesApp(
 ) {
     val groupedItems = viewModel.groupedItems.collectAsState()
     val query by viewModel.query
+    val context = LocalContext.current
+
     Box(modifier = modifier) {
         val scope = rememberCoroutineScope()
         val listState = rememberLazyListState()
@@ -96,6 +106,16 @@ fun JetHeroesApp(
                 }
             }
         }
+//        ScrollableAppBar(title = stringResource(R.string.app_name), navigationIcon = {
+//            IconButton(onClick = {
+//                Toast.makeText(context, "Menu clicked", Toast.LENGTH_SHORT).show()
+//            }) {
+//                Icon(
+//                    imageVector = Icons.Filled.Menu,
+//                    contentDescription = stringResource(R.string.menu)
+//                )
+//            }
+//        })
         AnimatedVisibility(
             visible = showButton,
             enter = fadeIn() + slideInVertically(),
@@ -111,6 +131,28 @@ fun JetHeroesApp(
                     }
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun ScrollableAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable (() -> Unit)? = null,
+    background: Color = MaterialTheme.colorScheme.primary,
+) {
+    Surface {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .background(color = background),
+        )
+        Row(modifier = modifier.padding(start = 12.dp)) {
+            if (navigationIcon != null) {
+                navigationIcon()
+            }
         }
     }
 }
